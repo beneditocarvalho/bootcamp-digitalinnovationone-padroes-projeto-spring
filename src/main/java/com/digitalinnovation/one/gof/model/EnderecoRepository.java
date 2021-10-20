f229d6b226316e0d90088e7eb7a7224e60dce77a
Benedito Carvalho <benedito.carvalho@gmail.com>
2021-10-20 10:17:57 -0300

    Added Model layer and its classes/interfaces

---
+++ src/main/java/com/digitalinnovation/one/gof/model/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.digitalinnovation.one.gof.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+
+}
