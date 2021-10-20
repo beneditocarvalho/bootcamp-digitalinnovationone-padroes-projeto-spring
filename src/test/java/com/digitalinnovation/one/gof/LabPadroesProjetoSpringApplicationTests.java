c673b6106515d9e2174e2e446f5623d84f85dc0e
Benedito Carvalho <benedito.carvalho@gmail.com>
2021-10-20 09:47:20 -0300

    First commit

---
+++ src/test/java/com/digitalinnovation/one/gof/LabPadroesProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.one.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPadroesProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
